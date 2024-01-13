9e7e502904bbbb89d9aa9911c70599f55a6025a6
Ruka-Chan <141768871+codex0555@users.noreply.github.com>
2024-01-13 22:11:21 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,17 +1,17 @@
 <!-- Start of Footer -->
-
+<img src="call_of_the_night.png" alt="" class="img-fluid call">
 <div class="container-fluid footer_outer">
     <div class="bg-dark box-footer">
         <div class="outro">
-            <!-- <img src="outro_footer1.png" alt="" class="img-fluid"> -->
+            <!-- <img src="call_of_the_night.png" alt="" class="img-fluid"> -->
             <div class="text-center policy">
                 <!-- Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab nobis voluptate facilis, asperiores fugiat officia sequi laboriosam quis, natus molestiae, est ut repudiandae fuga consectetur! -->
             </div>
             <div class="join_us">
                 <h4>Join Us On</h4>
-                <!-- <a href="#" class="1" id="faceb"><i class="fa-brands fa-facebook"></i> Facebook</a> -->
-                <a href="https://www.instagram.com/10th_fail.sayan/?hl=en" class="1" id="insta"><i class="fa-brands fa-instagram"></i> Insta</a>
-                <a href="#" class="1" id="telegram"> Get Back To Top.</a>
+                <a href="https://t.me/xyxanime_rf_gd" class="1" id="telegram"><i class="fa-brands fa-telegram"></i></a>
+                <a href="https://www.instagram.com/10th_fail.sayan/?hl=en" class="1" id="insta"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#" class="1" id="telegram"><i class="fa-solid fa-arrows-up-to-line"></i> Top</a>
             </div>
             <div class="clearfix"></div>
             <!-- <span><hr></span> -->
